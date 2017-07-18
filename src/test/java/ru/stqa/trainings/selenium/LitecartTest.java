2cbcea090e7dd3fcc43499556da036801a5dc016
tonio909 <anton.v.alekseev@yandex.ru>
2017-07-18 16:11:22 +0300

    Initial commit

---
+++ src/test/java/ru/stqa/trainings/selenium/LitecartTest.java
@@ -0,0 +1,39 @@
+package ru.stqa.trainings.selenium;
+
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+import java.util.concurrent.TimeUnit;
+
+
+public class LitecartTest {
+
+    private WebDriver driver;
+
+    @Before
+    public void start() {
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void litecartLogin() {
+        driver.navigate().to("http://localhost/litecart/admin/");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.xpath("//input[@name='remember_me']")).click();
+        driver.findElement(By.xpath("//button[@name='login']")).click();
+    }
+
+    @After
+    public void stop() {
+        driver.quit();
+        driver = null;
+    }
+}
